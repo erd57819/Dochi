87e45069a556e9b8fc7bc0cb14cea47d77692027
DESKTOP-DLOPF19\SSAFY <thdus1770@gmail.com>
2025-07-22 16:49:05 +0900

    feat : 프로젝트 생성

---
+++ SSAFY-DOCHI-BE/src/main/java/com/ssafy/dochi/SsafyDochiBeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyDochiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsafyDochiBeApplication.class, args);
+    }
+
+}
