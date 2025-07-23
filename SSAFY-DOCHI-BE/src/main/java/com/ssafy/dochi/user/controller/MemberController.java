9fbcd22bf7372b97c39dbd4997e477d02c8cfb95
DESKTOP-DLOPF19\SSAFY <thdus1770@gmail.com>
2025-07-23 17:03:10 +0900

    feat: test

---
+++ SSAFY-DOCHI-BE/src/main/java/com/ssafy/dochi/user/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.ssafy.dochi.user.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+}
