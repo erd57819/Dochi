9e411b8b732c3a84cc3975e04e6877293e8e9064
DESKTOP-DLOPF19\SSAFY <thdus1770@gmail.com>
2025-07-24 10:18:01 +0900

    feat : ApiResponse 구조 템플릿

---
+++ SSAFY-DOCHI-BE/src/main/java/com/ssafy/dochi/common/template/ApiResponse.java
@@ -0,0 +1,44 @@
+package com.ssafy.dochi.common.template;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+
+import java.io.Serializable;
+
+@Getter
+public class ApiResponse<B> extends ResponseEntity<B> {
+
+    public ApiResponse(final HttpStatus status){
+        super(status);
+    }
+
+    public ApiResponse(final B body, final HttpStatus status) {
+        super(body, status);
+    }
+
+    public ApiResponse(final B body, MultiValueMap<String, String> headers, HttpStatus status) {
+        super(body, headers, status);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class FailureCustomBody implements Serializable {
+
+        private int status;
+        private String message;
+        private String data;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class SuccesCustomBody<D> implements Serializable {
+        private int status;
+        private String message;
+        private D data;
+    }
+}
