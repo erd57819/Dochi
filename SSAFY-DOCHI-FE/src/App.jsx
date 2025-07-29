25991453c72e6572b74bf7d64f0039837121d69e
DESKTOP-DLOPF19\SSAFY <sw383@naver.com>
2025-07-30 02:25:35 +0900

    feat temp-video&router

--- SSAFY-DOCHI-FE/src/App.jsx
+++ SSAFY-DOCHI-FE/src/App.jsx
@@ -8,7 +8,7 @@ import NoticePage from './pages/NoticePage';
 import CommunityPage from './pages/CommunityPage';
 import ConflictCreatePage from './pages/ConflictCreatePage';
 import './App.css';
-
+import VideoCallRoom from './components/VideoCallRoom';
 const App = () => {
   return (
     <Router>
@@ -23,6 +23,7 @@ const App = () => {
             <Route path="/community" element={<CommunityPage />} />
             <Route path="/service" element={<ConflictCreatePage />} />
             <Route path="/conflict/create" element={<ConflictCreatePage />} />
+            <Route path="/video-call" element={<VideoCallRoom />} />
           </Routes>
         </main>
       </div>
