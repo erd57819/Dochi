825caa8843e7ccaa84a1b62f1ef6e2794bff013e
crew8264 <crew8264@gmail.com>
2025-08-09 15:00:08 +0900

    fix : config 파일에 .env가 매칭 안되는 오류 수정.

--- SSAFY-DOCJI-AI/core/config.py
+++ SSAFY-DOCJI-AI/core/config.py
@@ -6,40 +6,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 def to_env(name: str) -> str:
     return name.upper()
 
-# .env 경로 삽입
-ENV_PATH = Path(__file__).resolve().parents[2] / ".env"
+# # .env 경로 삽입
+# ENV_PATH = Path(__file__).resolve().parents[2] / ".env"
 
 class Settings(BaseSettings):
-    gms_key: str  # .env: GMS_KEY
-
-    mysql_root_password: str
-    jwt_secret: str
-    spring_profiles_active: str
-
-    email_username: str
-    email_password: str
-
-    kakao_rest_api_key: str
-    kakao_redirect_uri: str
-    kakao_redirect_withdraw_uri: str
-
-    aws_s3_bucket: str
-    aws_access_key: str
-    aws_secret_key: str
-
-    root_pw: str
-
-    livekit_api_key: str
-    livekit_api_secret: str
-
-    domain_or_public_ip: str
-    openvidu_secret: str
-    openvidu_url: str
-
-    certificate_type: str
-    https_port: str
-    http_port: str
-    social_pw: str
+    # .env: GMS_KEY
+    gms_key: str
 
     # 선택값(기본값 제공) — 없으면 빈 문자열
     google_application_credentials: str = ""
